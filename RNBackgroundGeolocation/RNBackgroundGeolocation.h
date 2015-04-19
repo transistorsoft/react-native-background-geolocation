3f7b43c9fc139cffc78617ac16bc0a45dcbde38d
Chris Scott <christocracy@gmail.com>
2015-04-19 09:13:00 -0400

    Initial Commit

---
+++ RNBackgroundGeolocation/RNBackgroundGeolocation.h
@@ -0,0 +1,13 @@
+//
+//  RNBackgroundGeolocation.h
+//  RNBackgroundGeolocation
+//
+//  Created by Christopher Scott on 2015-04-19.
+//  Copyright (c) 2015 Transistor Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNBackgroundGeolocation : NSObject
+
+@end
---
+++ RNBackgroundGeolocation/RNBackgroundGeolocation.m
@@ -0,0 +1,13 @@
+//
+//  RNBackgroundGeolocation.m
+//  RNBackgroundGeolocation
+//
+//  Created by Christopher Scott on 2015-04-19.
+//  Copyright (c) 2015 Transistor Software. All rights reserved.
+//
+
+#import "RNBackgroundGeolocation.h"
+
+@implementation RNBackgroundGeolocation
+
+@end

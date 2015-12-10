0f046afc87b2bb5b10be9ce9d740f194c628ef4c
Chris Scott <christocracy@gmail.com>
2015-12-10 18:00:08 -0500

    Refactor iOS to use multiple CLLocationManager instances for different types of location requests.  this allows getCurrentPosition to fetch a location from a dedicated CLLocationManager instance rather than pumping all locations through a single instance, which led to state-management nightmare

---
+++ RNBackgroundGeolocation/TSLocationManager.framework/Headers/LocationManager.h
@@ -0,0 +1,30 @@
+//
+//  CurrentPositionManager.h
+//  TSLocationManager
+//
+//  Created by Christopher Scott on 2015-12-07.
+//  Copyright © 2015 Transistor Software. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface LocationManager : NSObject <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager* locationManager;
+@property (strong, nonatomic) CLLocation* lastLocation;
+@property (strong, nonatomic) CLLocation* bestLocation;
+@property (nonatomic) NSInteger maxLocationAttempts;
+@property (nonatomic) CLLocationDistance distanceFilter;
+@property (nonatomic) CLLocationAccuracy desiredAccuracy;
+@property (nonatomic) CLActivityType activityType;
+@property (readonly) BOOL isUpdating;
+
+@property (copy) void (^locationChangedBlock) (LocationManager* manager, NSArray* locations, BOOL isSample);
+@property (copy) void (^errorBlock) (LocationManager* manager, NSError* error);
+
+-(void)startUpdatingLocation;
+-(void)startUpdatingLocation:(int)samples;
+-(void)stopUpdatingLocation;
+@end

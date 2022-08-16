fb082532adb8b40ca2fec6645ef70921df08b620
Wunsz <bjablonski@codeinthecup.pl>
2022-08-16 13:08:03 +0200

    Update activity key in MotionActivityEvent and migrate `string` to actual union type of possible activities.

--- src/declarations/interfaces/MotionActivityEvent.d.ts
+++ src/declarations/interfaces/MotionActivityEvent.d.ts
@@ -1,4 +1,19 @@
 declare module "react-native-background-geolocation" {
+  
+ /**
+  * The detected device motion activity.
+  *
+  * | Activity Name  |
+  * |----------------|
+  * | `still`        |
+  * | `walking`      |
+  * | `on_foot`      |
+  * | `running`      |
+  * | `on_bicycle`   |
+  * | `in_vehicle`   |
+  */
+  type Activity = 'still' | 'on_foot' | 'walking' | 'running' | 'in_vehicle' | 'on_bicycle';
+  
   /**
   * The event-object provided to [[BackgroundGeolocation.onActivityChange]].  Also attached to each recorded [[Location]].
   *
@@ -22,7 +37,7 @@ declare module "react-native-background-geolocation" {
     * | `on_bicycle`   |
     * | `in_vehicle`   |
     */
-    activity: string;
+    type: Activity;
     /**
     * Confidence of the reported device motion activity in %.
     */

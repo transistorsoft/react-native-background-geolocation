d19c1a7957122a64c219a3ecb7eb5e824a994559
Chris Scott <christocracy@gmail.com>
2026-01-16 14:04:40 -0500

    Add TSBackgroundFetch instructions

--- example/ios/BGGeoDemo/AppDelegate.swift
+++ example/ios/BGGeoDemo/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import TSBackgroundFetch
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     reactNativeDelegate = delegate
     reactNativeFactory = factory
-
+    
+    // [REQUIRED] Transistorsoft
+    TSBackgroundFetch.sharedInstance()?.didFinishLaunching()
+    
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
